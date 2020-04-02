2e8b6616682174cca22714b4c194bf5c97722a96
Sebastian Thomas <sebastianjthomas@gmail.com>
2020-04-02 17:06:57 +0100

    Installed Django

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-# response = requests.get('https://httpbin.org/ip')
+response = requests.get('https://httpbin.org/ip')
 
-# print('Your IP is {0}'.format(response.json()['origin']))
-result = requests.get("https://www.google.com/")
+print('Your IP is {0}'.format(response.json()['origin']))
+result = requests.get("https://www.whitehouse.gov/briefings-statements/")
 src = result.content
 soup = BeautifulSoup(src, 'lxml')
 
-# print(result.status_code)
-# print(result.headers)
+urls = []
+for h2_tag in soup.find_all('h2'):
+    a_tag = h2_tag.find('a')
+    # urls.append(a_tag.attrs['href'])
+    urls.append(a_tag.text)
 
-links = soup.find_all("a")
-for link in links:
-    if "About" in link.text:
-        print(link)
-        print(link.text)
-        print(link.attrs['href'])
+print(urls)
