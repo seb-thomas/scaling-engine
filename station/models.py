26c36b3275b51a1c4f90e6b0a056a190d5b3dd9f
Sebastian Thomas <sebastianjthomas@gmail.com>
2020-04-02 19:19:38 +0100

    Added station model

--- station/models.py
+++ station/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=120)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return "/stations/{}/".format(self.pk)
