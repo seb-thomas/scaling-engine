01502ee449759db1a4233eadae67aa85c9e932a8
Sebastian Thomas <sebastianjthomas@gmail.com>
2020-04-03 14:43:53 +0100

    Changed autopep to black, added url for stations

--- station/models.py
+++ station/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
 # Create your models here.
-
-
 class Station(models.Model):
     name = models.CharField(max_length=120)
     url = models.URLField()
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+    def get_delete_url(self):
+        return "/stations/{}/delete".format(self.pk)
+
+    def get_update_url(self):
+        return "/stations/{}/update".format(self.pk)
 
     def get_absolute_url(self):
         return "/stations/{}/".format(self.pk)
--- station/views.py
+++ station/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# from django.shortcuts import render, redirect
+# from .models import Station
+# from .forms import StationModelForm
 
-# Create your views here.
+# # Create your views here.
+# def create_view(request):
+#     form = StationModelForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         return redirect("/")
+
+#     context = {"form": form}
+
+#     return render(request, "station/create.html", context)
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
