a0bb1bc8a50378592e0f3f6326a28a46f736344f
Sebastian Thomas <sebastianjthomas@gmail.com>
2020-04-06 23:27:33 +0100

    added templates

--- station/views.py
+++ station/views.py
@@ -1,26 +1,16 @@
-# from django.shortcuts import render, redirect
-# from .models import Station
-# from .forms import StationModelForm
-
-# # Create your views here.
-# def create_view(request):
-#     form = StationModelForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         return redirect("/")
-
-#     context = {"form": form}
-
-#     return render(request, "station/create.html", context)
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 from .models import Station
 
 
 def index(request):
-    latest_question_list = Station.objects.all()[:5]
-    output = ", ".join([q.name for q in latest_question_list])
-    return HttpResponse(output)
+    station_list = Station.objects.all()[:5]
+    context = {
+        "station_list": station_list,
+    }
+    return render(request, "station/index.html", context)
 
 
 def detail(request, station_id):
-    return HttpResponse("Youre looking at %s. " % station_id)
+    station = get_object_or_404(Station, pk=station_id)
+    return render(request, "station/detail.html", {"station": station})
