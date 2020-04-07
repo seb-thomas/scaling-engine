34fad448ed18b00f9f2f69adc5de8332e77012e0
Sebastian Thomas <sebastianjthomas@gmail.com>
2020-04-07 07:46:15 +0100

    Added namespace

--- station/urls.py
+++ station/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "station"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:station_id>/", views.detail, name="detail"),
