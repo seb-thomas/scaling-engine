520de42d00afb46d9e66f63ed55cdb34b579b83c
Sebastian Thomas <sebastianjthomas@gmail.com>
2020-05-01 17:48:56 +0100

    Added colour logging

--- api/scraper/scraper/pipelines.py
+++ api/scraper/scraper/pipelines.py
@@ -5,7 +5,13 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy.exceptions import DropItem
+
 
 class ScraperPipeline:
     def process_item(self, item, spider):
-        return item
+        if item.get("text"):
+            if "better" in item["text"]:
+                return item
+            else:
+                raise DropItem("Not in %s" % item["author"])
