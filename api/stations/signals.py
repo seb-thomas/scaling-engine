87e54b67394d489454b537800d5682e697a94c21
Sebastian Thomas <sebastianjthomas@gmail.com>
2020-05-18 18:25:18 +0100

    Hooked celery task up to signal

--- api/stations/signals.py
+++ api/stations/signals.py
@@ -1,8 +1,11 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from .models import Episode
+from .tasks import call_func
 
 
 @receiver(post_save, sender=Episode)
 def episode_post_save(sender, instance, **kwargs):
-    print("Post save ep!")
+    if not instance.has_book:
+        call_func(1, 2)
+        print("Post save ep!")
--- api/stations/tasks.py
+++ api/stations/tasks.py
@@ -1,9 +1,11 @@
-from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
+logger = get_task_logger(__name__)
 
-@shared_task(name="sum_two_numbers")
+
+@shared_task(name="stations.tasks.call_func")
 def call_func(x, y):
     logger.info("Sent feedback email")
+    print("call_func")
     return x + y
