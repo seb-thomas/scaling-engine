02fa0edb334083d4200d7c41334588a8e9507429
seb <sebastian@leantech.me>
2025-11-20 20:15:56 +0000

    Fix blank page: add router setup to entry.client.tsx
    - React Router v7 plugin wasn't auto-injecting router code
    - Manually set up createBrowserRouter with all routes
    - Import all route components and loaders from app/routes/
    - This fixes the blank page issue where entry.client.js was empty

--- frontend/app/entry.client.tsx
+++ frontend/app/entry.client.tsx
@@ -1,4 +1,60 @@
 import { StrictMode, startTransition } from 'react'
 import { hydrateRoot } from 'react-dom/client'
-// React Router v7 plugin will inject the router here
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './root'
+import HomePage, { loader as homePageLoader } from './routes/_index'
+import AllBooksPage, { loader as allBooksPageLoader } from './routes/books'
+import AllShowsPage, { loader as allShowsPageLoader } from './routes/shows'
+import StationPage, { loader as stationPageLoader } from './routes/station.$stationId'
+import ShowPage, { loader as showPageLoader } from './routes/show.$showId'
+import BookDetailPage, { loader as bookDetailPageLoader } from './routes/book.$bookId'
+
+// Create router with file-based routes
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+        loader: homePageLoader,
+      },
+      {
+        path: 'books',
+        element: <AllBooksPage />,
+        loader: allBooksPageLoader,
+      },
+      {
+        path: 'shows',
+        element: <AllShowsPage />,
+        loader: allShowsPageLoader,
+      },
+      {
+        path: 'station/:stationId',
+        element: <StationPage />,
+        loader: stationPageLoader,
+      },
+      {
+        path: 'show/:showId',
+        element: <ShowPage />,
+        loader: showPageLoader,
+      },
+      {
+        path: 'book/:bookId',
+        element: <BookDetailPage />,
+        loader: bookDetailPageLoader,
+      },
+    ],
+  },
+])
+
+startTransition(() => {
+  hydrateRoot(
+    document.getElementById('root')!,
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  )
+})
 
