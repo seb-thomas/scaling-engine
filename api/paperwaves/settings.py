c1a8982047481c33b06e3056336f2f4a0bfecf12
seb <sebastian@leantech.me>
2025-11-20 20:03:48 +0000

    Force enable DEBUG mode in deployment for troubleshooting 502 errors

--- api/paperwaves/settings.py
+++ api/paperwaves/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# Temporarily enabled for debugging 502 errors
-DEBUG = int(os.environ.get("DEBUG", default=1))
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = [
